e66be6b4c2ec967937e659f3cfeb4ee5d5c228aa
Stefano Rivera <github@rivera.za.net>
2018-07-27 20:04:06 +0800

    Travis build #6 of debconf18/v0.0.0-2122-g1e24971 for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/stefanor/HDMI2USB-litex-firmware/tree/1e249716d8c34dda204f1cf449a528b1b29a66d2

---
+++ archive/debconf18/v0.0.0-2122-g1e24971/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "1e249716d8c34dda204f1cf449a528b1b29a66d2";
+const char* git_branch = "debconf18";
+const char* git_describe = "v0.0.0-2122-g1e24971";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
