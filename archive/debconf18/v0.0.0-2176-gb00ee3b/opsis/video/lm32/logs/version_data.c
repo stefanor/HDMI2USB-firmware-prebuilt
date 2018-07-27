dea2fa5fffb8186554b4842a3f81a541aecc763b
Stefano Rivera <github@rivera.za.net>
2018-07-27 17:42:26 +0800

    Travis build #5 of debconf18/v0.0.0-2176-gb00ee3b for PLATFORM=opsis TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/stefanor/HDMI2USB-litex-firmware/tree/b00ee3b8ee9d38e878750f7790c5d6dd811226e2

---
+++ archive/debconf18/v0.0.0-2176-gb00ee3b/opsis/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "b00ee3b8ee9d38e878750f7790c5d6dd811226e2";
+const char* git_branch = "debconf18";
+const char* git_describe = "v0.0.0-2176-gb00ee3b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
