bcba7daa1b63d3f2eec6193c9fd3b7475709c691
Stefano Rivera <github@rivera.za.net>
2018-07-27 19:48:06 +0800

    Travis build #6 of debconf18/v0.0.0-2122-g1e24971 for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/stefanor/HDMI2USB-litex-firmware/tree/1e249716d8c34dda204f1cf449a528b1b29a66d2

---
+++ archive/debconf18/v0.0.0-2122-g1e24971/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "1e249716d8c34dda204f1cf449a528b1b29a66d2";
+const char* git_branch = "debconf18";
+const char* git_describe = "v0.0.0-2122-g1e24971";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
