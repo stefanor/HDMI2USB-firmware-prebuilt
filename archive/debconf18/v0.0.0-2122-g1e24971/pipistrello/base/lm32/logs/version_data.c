5a3ee6414a54c7442bcf79e1fc76a624d50ec0a9
Stefano Rivera <github@rivera.za.net>
2018-07-27 19:51:09 +0800

    Travis build #6 of debconf18/v0.0.0-2122-g1e24971 for PLATFORM=pipistrello TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/stefanor/HDMI2USB-litex-firmware/tree/1e249716d8c34dda204f1cf449a528b1b29a66d2

---
+++ archive/debconf18/v0.0.0-2122-g1e24971/pipistrello/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "1e249716d8c34dda204f1cf449a528b1b29a66d2";
+const char* git_branch = "debconf18";
+const char* git_describe = "v0.0.0-2122-g1e24971";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
