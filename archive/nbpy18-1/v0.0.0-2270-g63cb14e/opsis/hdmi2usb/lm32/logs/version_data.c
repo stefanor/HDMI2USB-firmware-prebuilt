ddb3c977989431f7790491ed66bd45fb05f431ff
Stefano Rivera <github@rivera.za.net>
2018-11-02 20:51:40 -0700

    Travis build #8 of nbpy18-1/v0.0.0-2270-g63cb14e for PLATFORM=opsis TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/stefanor/HDMI2USB-litex-firmware/tree/63cb14e6de94105b1e93ebaefc665106f62294b2

---
+++ archive/nbpy18-1/v0.0.0-2270-g63cb14e/opsis/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "63cb14e6de94105b1e93ebaefc665106f62294b2";
+const char* git_branch = "nbpy18-1";
+const char* git_describe = "v0.0.0-2270-g63cb14e";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
