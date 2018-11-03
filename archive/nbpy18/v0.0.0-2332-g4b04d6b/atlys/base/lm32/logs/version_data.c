bc01ed85e97ff48fa437b31df618b4863f7f1935
Stefano Rivera <github@rivera.za.net>
2018-11-02 17:54:21 -0700

    Travis build #7 of nbpy18/v0.0.0-2332-g4b04d6b for PLATFORM=atlys TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/stefanor/HDMI2USB-litex-firmware/tree/4b04d6bc4318090a8d845b531b7d35393292e287

---
+++ archive/nbpy18/v0.0.0-2332-g4b04d6b/atlys/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "4b04d6bc4318090a8d845b531b7d35393292e287";
+const char* git_branch = "nbpy18";
+const char* git_describe = "v0.0.0-2332-g4b04d6b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
