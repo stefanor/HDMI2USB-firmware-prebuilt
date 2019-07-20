26ba3f8b7144fcfd75a306968cabbc68e39642be
Stefano Rivera <github@rivera.za.net>
2019-07-20 17:47:53 -0300

    Travis build #9 of debconf19/v0.0.0-2478-g8fbf094 for PLATFORM=opsis TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/stefanor/HDMI2USB-litex-firmware/tree/8fbf094dd0fbc68c4c892d7b146d15e6c0eb9ba6

---
+++ archive/debconf19/v0.0.0-2478-g8fbf094/opsis/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "8fbf094dd0fbc68c4c892d7b146d15e6c0eb9ba6";
+const char* git_branch = "debconf19";
+const char* git_describe = "v0.0.0-2478-g8fbf094";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
