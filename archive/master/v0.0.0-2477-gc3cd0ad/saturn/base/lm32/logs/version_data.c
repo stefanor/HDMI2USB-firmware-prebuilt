67fa388c2102690cf96978c35033ca7641cb6b82
Stefano Rivera <github@rivera.za.net>
2019-07-20 18:07:04 -0300

    Travis build #10 of master/v0.0.0-2477-gc3cd0ad for PLATFORM=saturn TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/stefanor/HDMI2USB-litex-firmware/tree/c3cd0ad331c88aa65d0e175f103af25bb7e433ad

---
+++ archive/master/v0.0.0-2477-gc3cd0ad/saturn/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_SATURN
+#error "Version mismatch - PLATFORM_SATURN not defined!"
+#endif
+const char* board = "saturn";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "c3cd0ad331c88aa65d0e175f103af25bb7e433ad";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.0-2477-gc3cd0ad";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
